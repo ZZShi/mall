97b6aabc794c84236e1f99876f79747015b46df1
ZZShi <zhuozhou.shi@longbridge.sg>
2023-09-15 09:56:00 +0800

    [ADD] - 封装侧边栏组件

--- mall_admin/src/layouts/components/FHeader.vue
+++ mall_admin/src/layouts/components/FHeader.vue
@@ -42,23 +42,55 @@
             </el-dropdown>
         </div>
     </div>
-    <el-drawer v-model="drawer" title="修改密码" size="45%">
-        <span>Hi there!</span>
-    </el-drawer>
+    <form-drawer ref="formDrawerRef" title="修改密码" destoryOnClose @submit="onSubmit">
+        <el-form ref="formRef" :rules="rules" :model="form" label-width="80px" size="small">
+            <el-form-item prop="oldpassword" label="旧密码">
+                <el-input v-model="form.oldpassword" placeholder="请输入旧密码">
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="password" label="新密码">
+                <el-input v-model="form.password" placeholder="请输入新密码" type="password">
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="repassword" label="确认密码">
+                <el-input v-model="form.repassword" placeholder="请输入确认密码" type="password">
+                </el-input>
+            </el-form-item>
+        </el-form>
+    </form-drawer>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import store from '~/store';
 import { showModal, toast } from '~/composables/util';
 import { useRouter } from 'vue-router';
 import { useFullscreen } from '@vueuse/core'
+import FormDrawer from '~/components/FormDrawer.vue';
+import { updatePassword } from '~/api/manage';
 
 const { isFullscreen, toggle } = useFullscreen()
 
 const router = useRouter()
+const form = ref({
+    "oldpassword": "admin",
+    "password": "12345",
+    "repassword": "12345"
+})
 
-const drawer = ref(false)
+const rules = reactive({
+    oldpassword: [
+        { required: true, message: '旧密码不能为空', trigger: 'blur' }
+    ],
+    password: [
+        { required: true, message: '新密码不能为空', trigger: 'blur' }
+    ],
+    repassword: [
+        { required: true, message: '确认密码不能为空', trigger: 'blur' }
+    ]
+})
+
+const formDrawerRef = ref(null)
 
 const handleRefresh = () => location.reload()
 
@@ -66,12 +98,24 @@ const handleCommand = (c) => {
     switch (c) {
         case "rePassword":
             console.log("Todo 修改密码")
-            drawer.value = true
+            formDrawerRef.value.open()
             break
         case "logout":
             handleLogout()
-            break
+            break 
     }
+
+}
+
+function onSubmit() {
+    formDrawerRef.value.showLoading()
+    updatePassword(form.value).then(res=>{
+        toast("修改密码成功")
+        store.dispatch("logout")
+        router.push("/login")
+    }).finally(()=>{
+        formDrawerRef.value.hideLoading()
+    })
 
 }
 
@@ -84,14 +128,13 @@ function handleLogout() {
             // 跳转回登录页
             router.push("/login")
         })
-
     }).catch(err => {
         console.log("取消退出登录")
     })
 }
 </script>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
 .f-header {
     @apply flex items-center bg-indigo-700 text-light-50 fixed top-0 left-0 right-0;
     height: 60px;
